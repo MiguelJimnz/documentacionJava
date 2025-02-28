dba9cbd7ee1ddefa4aa443ea8e3af11b026fe399
lorengie <angielojp56@gmail.com>
2025-02-27 21:57:34 -0500

    usando reverse en un repositorio reoto (:

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,6 +6,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Eliminando Ramas");
         System.out.println("Creando una rama en un repositorio remoto (:");
+        System.out.println("revertiendo cambios hechos");
 
 
 
